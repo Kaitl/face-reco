ad81691fd5acf66ecb7f2cad29dd6da638422793
Kaitl <117489101+Kaitl@users.noreply.github.com>
2024-04-22 19:29:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-https://teachablemachine.withgoogle.com/models/bAqkno8mp/
-
-
 Webcam.set({
     width: 400,
     height: 300,
@@ -17,3 +14,25 @@ function Capture()
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }   
+function modelLoaded(){
+    console.log('Model Loaded!')
+}
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bAqkno8mp/model.json', modelLoaded)
+
+console.log('ml5 version:',ml5.version)
+
+function Rawr(){
+    img1=document.getElementById('captured_image')
+    classifier.classify(img1, getResult)
+}
+
+function getResult(error, results){
+    if (error) {
+        console.error(error)
+    } else{
+        console.log(results)
+        document.getElementById("object").innerHTML="Object: "+results[0].label
+        document.getElementById("accuracy").innerHTML="Accuracy:"+results[0].confidence.toFixed(2)
+    }
+}
